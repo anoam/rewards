e77e194a919aeddc490eb741600afbdc3eeec662
anoam <anohinam@gmail.com>
2019-02-03 20:07:34 +0300

    Domain implementation

--- lib/domain/customer_repository.rb
+++ lib/domain/customer_repository.rb
@@ -1,8 +1,31 @@
 # frozen_string_literal: true
+require 'set'
 
 module Domain
   # Abstraction over a collection of [Customer]s
   class CustomerRepository
+    def initialize
+      @collection = Set.new
+    end
 
+    # @param name [String] name of the Customer to find
+    # @return [Customer] the customer with given name or nil if the Customer wasn't found
+    def find_by_name(name)
+      collection.find { |customer| customer.name == name }
+    end
+
+    # Adds a customer to the collection
+    # @param name [Customer] customer to add into collection
+    def add(customer)
+      collection.add(customer)
+    end
+
+    # @return [Array<Customer>] all stored customers
+    def all
+      collection.to_a
+    end
+
+    private
+    attr_reader :collection
   end
 end
--- lib/domain/recommendation_service.rb
+++ lib/domain/recommendation_service.rb
@@ -3,6 +3,27 @@
 module Domain
   # Service that represents recommendation process
   class RecommendationService
+    # @param customer_repository [Domain::CustomerRepository] repository of known customers
+    def initialize(customer_repository:)
+      @repository = customer_repository
+    end
 
+    # @param referrer [Domain::Customer] a customer who has invited the new one
+    # @param referred_name [String] name of the recommended user
+    def recommend(referrer:, referred_name:)
+      return if repository.find_by_name(referred_name)
+
+      new_customer = build_referred_customer(referred_name, referrer)
+      repository.add(new_customer)
+    end
+
+    private
+    attr_reader :repository
+
+    def build_referred_customer(name, referrer)
+      new_customer = Customer.new(name: name, accepted: false)
+      new_customer.referrer = referrer
+      new_customer
+    end
   end
 end
